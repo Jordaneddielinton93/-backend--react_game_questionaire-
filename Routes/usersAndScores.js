f5cfe560a0153ef4676a94e39bf669f453a95559
Jordan Linton <jordaneddielinton93@hotmail.co.uk>
2021-12-21 22:05:15 +0000

    added a new put route for updating the topscores

--- Routes/usersAndScores.js
+++ Routes/usersAndScores.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllUsersAndScores } from "../models/usersAndScores/getAllUsers.js";
+import { getAllUsersAndScores, updateUsersAndScores } from "../models/usersAndScores/getAllUsers.js";
 
 
 let Router= express.Router()
@@ -9,6 +9,16 @@ Router.get("/",async (req,res)=>{
   let allUsers = await getAllUsersAndScores()
   
   res.json(allUsers)
+})
+
+Router.put("/:id",async (req,res)=>{
+  // let {}=req.body
+  let {id}=req.params
+  let {user_name,user_score}=req.body
+  console.log(user_name,user_score,id)
+  await updateUsersAndScores(user_name,user_score,id)
+  
+  res.json({message:"successfull"})
 })
 
 export default Router
--- models/usersAndScores/getAllUsers.js
+++ models/usersAndScores/getAllUsers.js
@@ -3,7 +3,14 @@ import query from "../../db/index.js";
  export async function getAllUsersAndScores(){
   console.log("hit")
   let results= await query(`SELECT * FROM userscores;`)
-  console.log("hit again")
+  console.log(results)
+  return results.rows
+}
+
+export async function updateUsersAndScores(user_name,user_score,user_id){
+  console.log("hit")
+  let results= await query(`UPDATE userscores SET user_name=$1,user_score=$2 WHERE user_id=$3 RETURNING *;`,[user_name,user_score,user_id])
+  console.log(results)
   return results.rows
 }
 
